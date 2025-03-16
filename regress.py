de434902b90f683c949982761820d3ec0fc747f4
Samarth Sinha <samarth9038@gmail.com>
2025-03-16 23:22:13 +0530

    Upgradation to Polynomial regression

--- regress.py
+++ regress.py
@@ -9,17 +9,19 @@ def standardization(dataset):
   return dataset, mean, deviation
 
 
-def model(new_Data, theta, mean, deviation):
+def model(new_Data, theta, mean, deviation, degree=2):
   new_Data = np.array([[float(new_Data)]])
   new_Data = (new_Data-mean)/deviation
+  new_Data = np.column_stack([new_Data**i for i in range(degree+1)])
   new_Data = np.column_stack((np.ones(len(new_Data)), new_Data))
   res = np.dot(new_Data, theta)
-  return int(round(float(res[0][0]),2)*100)
+  print(res)
+  return int(round(float(res[0]),2)*100)
 
 
 def gradient_descent(dataset, y_target, alpha, iterations):
   m = len(dataset) 
-  theta = np.array([[1.0], [0.15302957]])
+  theta = np.zeros(dataset.shape[1])
 
   for i in range(iterations):
     prediction = np.dot(dataset, theta)
@@ -29,27 +31,38 @@ def gradient_descent(dataset, y_target, alpha, iterations):
 
   return theta
 
-def newWeights():
-    data = pd.read_csv('allData.csv', header = None, index_col=None, usecols=[0])
-    dataset = np.array(data)
-    y_Trgt = np.array(pd.read_csv('allData.csv', header = None, index_col=None, usecols=[1]))
-    alpha = 0.01
-    iterations = 10000
+def newWeights(degree=2, alpha=0.01, iterations=10000):
+    #Linear Regression Upgraded to Polynomial Regression
+    # data = pd.read_csv('allData.csv')
+    # y_Trgt = data['val']
+    # data = data['dist']
+    # alpha = 0.01
+    # iterations = 10000
+    # dataset, mean, dev = standardization(data)
+    # plt.scatter(y_Trgt, dataset)
+    # plt.show()
+    # dataset = np.column_stack((np.ones(len(dataset)), dataset))
+    # weights = gradient_descent(dataset, y_Trgt, alpha, iterations)
+    # np.savez('data.npz', mean = mean, dev = dev, out = weights)
 
-    m = len(dataset)
-    dataset, mean, dev = standardization(dataset)
-    dataset = np.column_stack((np.ones(len(dataset)), dataset))
-    weights = gradient_descent(dataset, y_Trgt, alpha, iterations)
+    data = pd.read_csv('allData.csv')
+    y_Trgt = data['val'].values
+    data = data['dist'].values.reshape(-1, 1)
 
-    # out = model(4, weights, mean, dev)
-    np.savez('data.npz', mean = mean, dev = dev, out = weights)
-    # print(out)
+    dataset, mean, dev = standardization(data)
+    dataset_poly = np.column_stack([dataset**i for i in range(degree + 1)])
+    dataset_poly = np.column_stack((np.ones(len(dataset_poly)), dataset_poly))
+
+    weights = gradient_descent(dataset_poly, y_Trgt, alpha, iterations)
+    np.savez('data.npz', mean=mean, dev=dev, out=weights, degree = degree)
+
+    return weights
 
 
 def call(number):
     try:
-        data = np.load('data.npz')
-        return (model(number, data['out'], data['mean'], data['dev']))
+      data = np.load('data.npz')
+      return (model(number, data['out'], data['mean'], data['dev']))
 
     except:
        print("NO OLD DATA, EXITING")
@@ -58,4 +71,5 @@ def call(number):
        
 
 # if __name__ == '__main__':
-   # print(call(5)
+#    newWeights()
+#    print(call(2))
